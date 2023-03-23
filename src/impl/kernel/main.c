8ee10832ab85f7307ddf1b8fef70d6e690955ab1
ClaraCF <78223334+ClaraCF@users.noreply.github.com>
2023-03-23 05:04:17 -0400

    Update main.c to reflect function name changes

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -4,5 +4,5 @@ void kernel_main(){
     // Welcome message
     print_clear();
     print_set_color(PRINT_COLOR_PURPLE, PRINT_COLOR_BLACK);
-    print_str("Welcome to KlarityOS!\n\n");
+    print_string("Welcome to KlarityOS!\n\n");
 }
